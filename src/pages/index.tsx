2d8b8a575615198e087cb8d1620c4a44aa4505dd
Lucas Ramos <ramos.lucash@gmail.com>
2022-05-01 22:04:35 -0300

    implement home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,29 @@ import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
+import { AxiosResponse } from 'axios';
 import { Header } from '../components/Header';
 import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+
+type ImagesResponse = {
+  data: {
+    title: string;
+    description: string;
+    url: string;
+    ts: number;
+    id: string;
+  }[];
+  after: string;
+};
+
+const fetchImages = ({
+  pageParam = null,
+}): Promise<AxiosResponse<ImagesResponse>> => {
+  return api.get('/api/images', { params: { after: pageParam } });
+};
 
 export default function Home(): JSX.Element {
   const {
@@ -16,20 +34,21 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage.data.after || null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.map(item => item.data.data).flat();
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +56,14 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            isLoading={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+          >
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
