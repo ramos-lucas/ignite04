2e93138fb0c0af4812fe58bb2a253afe13e9580b
Lucas Ramos <ramos.lucash@gmail.com>
2022-05-01 22:05:03 -0300

    implement image modal

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,20 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="2xl">
+      <ModalOverlay />
+
+      <ModalContent bgColor="pGray.800">
+        <ModalBody>
+          <Image src={imgUrl} maxW="900px" maxH="600px" margin="auto" />
+        </ModalBody>
+        <ModalFooter justifyContent="flex-start">
+          <Link href={imgUrl} target="_blank">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
